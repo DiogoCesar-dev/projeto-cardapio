740e02cc7ccc2f5cf0c46a16d62ffbaf7f6f86e3
Guilherme Gutierrez Medeiros <03345450135@sinfra.mt.gov.br>
2025-03-19 09:35:58 -0400

    Finalizando projeto

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
 const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
-const addresswarn = document.getElementById("address-warn")
+const addressWarn = document.getElementById("address-warn")
 
 
 let cart = [];
 
 // Abri o modal do carrinho 
 cartBtn.addEventListener("click", function () {
-    updateCartModal();
     cartModal.style.display = "flex"
+    updateCartModal();
 })
 
 //Fechar o modal quando clicar fora
@@ -46,6 +46,18 @@ menu.addEventListener("click", function (event) {
 function addToCart(name, price) {
 
     const existingItem = cart.find(item => item.name === name)
+
+    Toastify({
+        text: "Adicionado ao carrinho!",
+        duration: 4000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "green",
+        },
+    }).showToast();
 
     if (existingItem) {
         //se o item ja existe, aumenta apenas a quantidade + 1
@@ -77,11 +89,11 @@ function updateCartModal() {
             <div class="flex items-center justify-between">
                <div>
                 <p class="font-medium">${item.name}</p>
-                <p>Qtd: ${item.quantity}</p>
+                <p>Quantidade: ${item.quantity}</p>
                 <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
                </div> 
            
-                    <button class="remove-from-cart-btn data-name="${item.name}">
+                    <button class="remove-from-cart-btn" data-name="${item.name}">
                         Remover
                     </button>
              
@@ -105,8 +117,97 @@ function updateCartModal() {
 
 //função remover item do carrinho
 
-cartItemsContainer.addEventListener("click", function(event){
+cartItemsContainer.addEventListener("click", function (event){
     if(event.target.classList.contains("remove-from-cart-btn")){
         const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
     }
 })
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input",function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+//Finalizar pedido
+checkoutBtn.addEventListener("click", function(){
+    
+    const isOpen = checkRestauranteOpen();
+    if(!isOpen){
+        Toastify({
+            text: "Ops, o restaurante está fechado!",
+            duration: 4000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#ef4444",
+            },
+        }).showToast();
+        return;
+    }
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ''){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+//ENVIAR O PEDIDO PARA API WHATSAPP WEB
+    const cartItems = cart.map((item) => {
+        return (
+            ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price}|`)
+    }).join("")
+    
+    const message = encodeURIComponent(cartItems)
+    const phone ="65993481587"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
+
+})
+
+// Verificar a hora e manipular o card horario
+function checkRestauranteOpen() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 23;
+    //true = restaurante está aberto
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestauranteOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-500")
+}else{
+    spanItem.classList.remove("bg-green-500")
+    spanItem.classList.add("bg-red-500")
+}
